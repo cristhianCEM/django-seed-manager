6ba63c6c45a717beca42bfdb0d1282154abb331a
cristhianCEM <cristianbcer27@gmail.com>
2024-11-12 01:50:13 -0600

    fix: add more methods to SeedFaker

--- django_seed_manager/core/seed_faker.py
+++ django_seed_manager/core/seed_faker.py
@@ -1,8 +1,9 @@
-from random import randint
+from random import randint, choice
 from datetime import datetime, timedelta, timezone
 from faker import Faker
 
 DEFAULT_TIMEZONE_OFFSET = -6
+BOOLEANS = [True, False]
 
 
 class SeedFaker:
@@ -10,6 +11,12 @@ class SeedFaker:
         self.fake = Faker()
         self.timezone_hours_offset = timezone_hours_offset
         self.timezone_delta = timedelta(hours=self.timezone_hours_offset)
+
+    def boolean(self) -> bool:
+        return choice(BOOLEANS)
+
+    def decision(self) -> bool:
+        return choice(BOOLEANS)
 
     def date_time_future(self, min_days=1, max_days=10):
         today = datetime.now(timezone.utc) + self.timezone_delta
@@ -20,3 +27,15 @@ class SeedFaker:
 
     def date_time_between_years(self, min_year: int, max_year: int):
         return self.fake.date_time_between(start_date=f"{min_year}-01-01", end_date=f"{max_year}-12-31")
+
+    def name(self, uppercase: bool = False):
+        result = self.fake.name()
+        return result.upper() if uppercase else result
+
+    def address(self, uppercase: bool = False):
+        result = self.fake.address()
+        return result.upper() if uppercase else result
+
+    def city(self, uppercase: bool = False):
+        result = self.fake.city()
+        return result.upper() if uppercase else result
