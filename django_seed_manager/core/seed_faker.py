2180529eeda3e32f049c490901237025ce86c0b3
cristhianCEM <cristianbcer27@gmail.com>
2024-11-12 01:29:15 -0600

    Create seed_faker.py

---
+++ django_seed_manager/core/seed_faker.py
@@ -0,0 +1,22 @@
+from random import randint
+from datetime import datetime, timedelta, timezone
+from faker import Faker
+
+DEFAULT_TIMEZONE_OFFSET = -6
+
+
+class SeedFaker:
+    def __init__(self, timezone_hours_offset=DEFAULT_TIMEZONE_OFFSET):
+        self.fake = Faker()
+        self.timezone_hours_offset = timezone_hours_offset
+        self.timezone_delta = timedelta(hours=self.timezone_hours_offset)
+
+    def date_time_future(self, min_days=1, max_days=10):
+        today = datetime.now(timezone.utc) + self.timezone_delta
+        return today + timedelta(days=randint(min_days, max_days))
+
+    def date_between_years(self, min_year: int, max_year: int):
+        return self.fake.date_between(start_date=f"{min_year}-01-01", end_date=f"{max_year}-12-31")
+
+    def date_time_between_years(self, min_year: int, max_year: int):
+        return self.fake.date_time_between(start_date=f"{min_year}-01-01", end_date=f"{max_year}-12-31")
